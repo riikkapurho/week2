5f7f6d1d06cfd874cc495f1298033c4f329a51ad
riikkapurho <113058359+riikkapurho@users.noreply.github.com>
2022-09-11 22:28:07 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+let button = document.getElementById("submit-data");
+button.addEventListener("click", function () {
+  var table = document.getElementById("table");
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
+
+  cell1.innerHTML = document.getElementById("input-username").value;
+
+  cell2.innerHTML = document.getElementById("input-email").value;
+
+  cell3.innerHTML = document.getElementById("input-address").value;
+
+  cell4.innerHTML = document.getElementById("input-admin").value;
+});
